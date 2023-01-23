8e06014b04b712a19c4e891f9e5cfb26c15ea37f
eaakre <erikaakre@hotmail.com>
2023-01-22 19:00:15 -0600

    upload files

---
+++ main.js
@@ -0,0 +1,83 @@
+let homeDropdown = document.getElementById("home-dropdown");
+let awayDropdown = document.getElementById("away-dropdown");
+let homeLabel = document.getElementById("home-label");
+let awayLabel = document.getElementById("away-label");
+let homeTeamCard = document.getElementById("homeTeamImg");
+let awayTeamCard = document.getElementById("awayTeamImg");
+let homePG = document.getElementById("home-pg");
+let homeSG = document.getElementById("home-sg");
+let homeSF = document.getElementById("home-sf");
+let homePF = document.getElementById("home-pf");
+let homeC = document.getElementById("home-c");
+let awayPG = document.getElementById("away-pg");
+let awaySG = document.getElementById("away-sg");
+let awaySF = document.getElementById("away-sf");
+let awayPF = document.getElementById("away-pf");
+let awayC = document.getElementById("away-c");
+
+
+
+// Populate Home Dropdown from teams.js
+for (let i = 0; i < team.length; i++) {
+    let teamName = team[i].city;
+    let option = document.createElement('option');
+    option.textContent = teamName;
+    option.value = teamName;
+
+    homeDropdown.appendChild(option)
+}
+
+// Populate Away Dropdown from teams.js
+for (let i = 0; i < team.length; i++) {
+    let teamName = team[i].city;
+    let option = document.createElement('option');
+    option.textContent = teamName;
+    option.value = teamName;
+    awayDropdown.appendChild(option)
+}
+
+// Use value from selected options on dropdowns to change lineup cards
+function showCards(side, option) {
+    let selction = `${side}: ${option}`
+    let teamFile = option.toLowerCase();
+    let roster = [];
+    for (i=0; i < player.length; i++) {
+        if (player[i].team === option) {
+            roster.push(player[i])
+        }
+    }
+
+    if (side === "Home") {
+        homeLabel.innerText = selction;
+        homeTeamCard.src = `teamcards/${teamFile}.png`;
+        for (i=0; i < 5; i++) {
+            if (roster[i].position == 1) {
+                homePG.src = roster[i].card;
+            } else if (roster[i].position == 2) {
+                homeSG.src = roster[i].card;
+            } else if (roster[i].position == 3) {
+                homeSF.src = roster[i].card
+            } else if (roster[i].position == 4) {
+                homePF.src = roster[i].card
+            } else {
+                homeC.src = roster[i].card;
+            }
+        }
+    } else {
+        awayLabel.innerText = selction
+        awayTeamCard.src = `teamcards/${teamFile}.png`;
+        for (i=0; i < 5; i++) {
+            if (roster[i].position == 1) {
+                awayPG.src = roster[i].card;
+            } else if (roster[i].position == 2) {
+                awaySG.src = roster[i].card;
+            } else if (roster[i].position == 3) {
+                awaySF.src = roster[i].card
+            } else if (roster[i].position == 4) {
+                awayPF.src = roster[i].card
+            } else {
+                awayC.src = roster[i].card;
+            }
+        }
+    }
+}
---
+++ teams.js
@@ -0,0 +1,164 @@
+let team = [
+    {
+        city: 'Atlanta',
+        nickname: 'Hawks',
+        conference: 'East',
+        card: 'teamcards/atlanta.png'
+    },
+    {
+        city: 'Boston',
+        nickname: 'Celtics',
+        conference: 'East',
+        card: 'teamcards/boston.png'
+    },
+    {
+        city: 'Charlotte',
+        nickname: 'Hornets',
+        conference: 'East',
+        card: 'teamcards/charlotte.png'
+    },
+    {
+        city: 'Chicago',
+        nickname: 'Bulls',
+        conference: 'East',
+        card: 'teamcards/chicago.png'
+    },
+    {
+        city: 'Cleveland',
+        nickname: 'Cavaliers',
+        conference: 'East',
+        card: 'teamcards/cleveland.png'
+    },
+    {
+        city: 'Dallas',
+        nickname: 'Mavericks',
+        conference: 'West',
+        card: 'teamcards/dallas.png'
+    },
+    {
+        city: 'Denver',
+        nickname: 'Nuggets',
+        conference: 'West',
+        card: 'teamcards/denver.png'
+    },
+    {
+        city: 'Detroit',
+        nickname: 'Pistons',
+        conference: 'East',
+        card: 'teamcards/detroit.png'
+    },
+    {
+        city: 'Golden-State',
+        nickname: 'Warriors',
+        conference: 'West',
+        card: 'teamcards/golden-state.png'
+    },
+    {
+        city: 'Houston',
+        nickname: 'Rockets',
+        conference: 'West',
+        card: 'teamcards/houston.png'
+    },
+    {
+        city: 'Indiana',
+        nickname: 'Pacers',
+        conference: 'East',
+        card: 'teamcards/indiana.png'
+    },
+    {
+        city: 'LAC',
+        nickname: 'Clippers',
+        conference: 'West',
+        card: 'teamcards/lac.png'
+    },
+    {
+        city: 'LAL',
+        nickname: 'Lakers',
+        conference: 'West',
+        card: 'teamcards/lal.png'
+    },
+    {
+        city: 'Miami',
+        nickname: 'Heat',
+        conference: 'East',
+        card: 'teamcards/miami.png'
+    },
+    {
+        city: 'Milwaukee',
+        nickname: 'Bucks',
+        conference: 'East',
+        card: 'teamcards/milwaukee.png'
+    },
+    {
+        city: 'Minnesota',
+        nickname: 'Timberwolves',
+        conference: 'West',
+        card: 'teamcards/minnesota.png'
+    },
+    {
+        city: 'New-Jersey',
+        nickname: 'Nets',
+        conference: 'East',
+        card: 'teamcards/new-jersey.png'
+    },
+    {
+        city: 'New-York',
+        nickname: 'Knicks',
+        conference: 'East',
+        card: 'teamcards/new-york.png'
+    },
+    {
+        city: 'Orlando',
+        nickname: 'Magic',
+        conference: 'East',
+        card: 'teamcards/orlando.png'
+    },
+    {
+        city: 'Philadelphia',
+        nickname: '76ers',
+        conference: 'East',
+        card: 'teamcards/philadelphia.png'
+    },
+    {
+        city: 'Phoenix',
+        nickname: 'Suns',
+        conference: 'West',
+        card: 'teamcards/phoenix.png'
+    },
+    {
+        city: 'Portland',
+        nickname: 'Trail Blazers',
+        conference: 'West',
+        card: 'teamcards/portland.png'
+    },
+    {
+        city: 'San-Antonio',
+        nickname: 'Spurs',
+        conference: 'West',
+        card: 'teamcards/san-antonio.png'
+    },
+    {
+        city: 'Sacramento',
+        nickname: 'Kings',
+        conference: 'West',
+        card: 'teamcards/sacramento.png'
+    },
+    {
+        city: 'Seattle',
+        nickname: 'SuperSonics',
+        conference: 'West',
+        card: 'teamcards/seattle.png'
+    },
+    {
+        city: 'Utah',
+        nickname: 'Jazz',
+        conference: 'West',
+        card: 'teamcards/utah.png'
+    },
+    {
+        city: 'Washington',
+        nickname: 'Bullets',
+        conference: 'East',
+        card: 'teamcards/washington.png'
+    },
+]
