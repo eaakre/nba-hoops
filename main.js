959f5c89e1970113c6f40b63e19a83e9cb59ac98
eaakre <erikaakre@hotmail.com>
2023-01-28 18:43:58 -0600

    player stats to gallery view

--- main.js
+++ main.js
@@ -36,11 +36,30 @@ let awayC = document.getElementById("away-c");
 // Open Gallery view of player cards
 let fullImgBox = document.getElementById("fullImgBox");
 let fullImg = document.getElementById("fullImg");
-let playerStats = document.getElementById("playerStats")
+let playerStats = document.getElementById("playerStats");
+let fullStats = document.getElementById("fullStats");
+let playerName = document.getElementById("player-name");
+let playerTeam = document.getElementById("player-team");
+let playerPoints = document.getElementById("player-points");
+let playerRebounds = document.getElementById("player-rebounds");
+let playerAssists = document.getElementById("player-assists");
+let playerSteals = document.getElementById("player-steals");
+let playerBlocks = document.getElementById("player-blocks");
+let playerPPG = document.getElementById("player-ppg");
 
-function openFullImg(img) {
+function openFullImg(img, player) {
     fullImg.src = img;
     fullImgBox.style.display = "flex";
+    fullStats.style.display = "flex";
+    console.log(player)
+    playerName.innerHTML = `${player.fullname}`;
+    playerTeam.innerHTML = `Team: ${player.team}`;
+    playerPoints.innerHTML = `Points: ${player.points}`;
+    playerRebounds.innerHTML = `Rebounds: ${player.rebounds}`;
+    playerAssists.innerHTML = `Assists: ${player.assists}`;
+    playerSteals.innerHTML = `Steals: ${player.steals}`;
+    playerBlocks.innerHTML = `Blocks: ${player.blocks}`;
+    playerPPG.innerHTML = `PPG: ${player.ppg}`;
 }
 
 function closeFullImg() {
@@ -88,14 +107,19 @@ function showCards(side, option) {
         for (i=0; i < 5; i++) {
             if (roster[i].position == 1) {
                 homePG.src = roster[i].card;
+                homePG.value = roster[i]
             } else if (roster[i].position == 2) {
                 homeSG.src = roster[i].card;
+                homeSG.value = roster[i]
             } else if (roster[i].position == 3) {
                 homeSF.src = roster[i].card
+                homeSF.value = roster[i]
             } else if (roster[i].position == 4) {
                 homePF.src = roster[i].card
+                homePF = roster[i]
             } else {
                 homeC.src = roster[i].card;
+                homeC.value = roster[i]
             }
         }
     } else {
@@ -104,14 +128,19 @@ function showCards(side, option) {
         for (i=0; i < 5; i++) {
             if (roster[i].position == 1) {
                 awayPG.src = roster[i].card;
+                awayPG.value = roster[i];
             } else if (roster[i].position == 2) {
                 awaySG.src = roster[i].card;
+                awaySG.value = roster[i];
             } else if (roster[i].position == 3) {
-                awaySF.src = roster[i].card
+                awaySF.src = roster[i].card;
+                awaySF.value = roster[i];
             } else if (roster[i].position == 4) {
-                awayPF.src = roster[i].card
+                awayPF.src = roster[i].card;
+                awayPF.value = roster[i];
             } else {
                 awayC.src = roster[i].card;
+                awayC.value = roster[i];
             }
         }
     }
